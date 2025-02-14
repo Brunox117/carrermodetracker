5890a06db479ce299e6bc6d5b84b4506a1ebcdcd
Bruno Jimenez <omarjm0910@gmail.com>
2025-02-14 13:18:17 -0600

    Se empieza a trabajar el widget de equipos

--- lib/presentation/views/home/home_view.dart
+++ lib/presentation/views/home/home_view.dart
@@ -1,17 +1,36 @@
+import 'package:carrermodetracker/presentation/widgets/home/team_general_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+List<TeamGeneralWidget> listaPrueba = [
+  const TeamGeneralWidget(
+    nombreEquipo: 'BVB',
+  ),
+  const TeamGeneralWidget(
+    nombreEquipo: 'Real Madrid',
+  ),
+  const TeamGeneralWidget(
+    nombreEquipo: 'FCB',
+  ),
+];
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final appLocalizations = AppLocalizations.of(context)!;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-      ),
-      body: const Center(
-        child: Text('HomeView'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(appLocalizations.home_view_title),
+        ),
+        body: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2),
+          itemCount: listaPrueba.length,
+          itemBuilder: (context, index) {
+            return listaPrueba[index];
+          },
+        ));
   }
 }
---
+++ lib/presentation/widgets/home/team_general_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class TeamGeneralWidget extends StatelessWidget {
+  final String nombreEquipo;
+  const TeamGeneralWidget({super.key, required this.nombreEquipo});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      decoration: BoxDecoration(
+          color: Colors.lightBlue.withOpacity(.6),
+          borderRadius: BorderRadius.circular(20)),
+      child: Stack(
+        children: [
+          const SizedBox.expand(),
+          const Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 30,
+            child: Icon(
+              size: 100,
+              Icons.circle,
+            ),
+          ),
+          Positioned(
+              left: 0,
+              right: 0,
+              bottom: 20,
+              child: Center(
+                child: Text(
+                  nombreEquipo,
+                ),
+              ))
+        ],
+      ),
+    );
+  }
+}
