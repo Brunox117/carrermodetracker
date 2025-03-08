c47b02fffb08eed476fef2f9a7583e808c3c6a81
Bruno Jimenez <omarjm0910@gmail.com>
2025-03-08 14:29:22 -0600

    se crea la enumeracion posiciones y se empieza el formulario

---
+++ lib/domain/enums/positions.dart
@@ -0,0 +1,12 @@
+enum Positions {
+  dc, 
+  ei,
+  ed,
+  mco,
+  mc,
+  mcd,
+  dfc,
+  dfd,
+  dfi,
+  po
+}
--- lib/presentation/views/team/add_player_view.dart
+++ lib/presentation/views/team/add_player_view.dart
@@ -1,7 +1,10 @@
-import 'package:carrermodetracker/domain/entities/player.dart';
-import 'package:carrermodetracker/presentation/providers/players/players_provider.dart';
+import 'package:carrermodetracker/presentation/widgets/forms/custom_form_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'package:carrermodetracker/domain/entities/player.dart';
+import 'package:carrermodetracker/presentation/providers/players/players_provider.dart';
 
 class AddPlayerView extends StatelessWidget {
   final String id;
@@ -13,17 +16,14 @@ class AddPlayerView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Agregar jugador'),
       ),
-      body: const Column(
-        children: [
-          _PlayerForm(),
-        ],
-      ),
+      body: _PlayerForm(teamId: id),
     );
   }
 }
 
 class _PlayerForm extends ConsumerStatefulWidget {
-  const _PlayerForm();
+  final String teamId;
+  const _PlayerForm({required this.teamId});
 
   @override
   ConsumerState<ConsumerStatefulWidget> createState() => __PlayerFormState();
@@ -34,8 +34,35 @@ class __PlayerFormState extends ConsumerState<_PlayerForm> {
     ref.read(playersProvider.notifier).addPlayer(player);
   }
 
+  final _formKey = GlobalKey<FormState>();
+  final imagePicker = ImagePicker();
+
+  String name = '';
+  String number = '';
+  String position = '';
+  String imageURL = '';
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              CustomFormField(
+                isTopField: true,
+                hint: "Nombre del jugador",
+                onChanged: (value) => name = value,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'El nombre es requerido';
+                  }
+                  return null;
+                },
+              ),
+            ],
+          )),
+    );
   }
 }
