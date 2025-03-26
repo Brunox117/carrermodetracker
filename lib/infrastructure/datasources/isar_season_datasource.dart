a2187d70c2c24e003c0b9b272ef8e08af578729c
Bruno Jimenez <omarjm0910@gmail.com>
2025-03-26 10:33:15 -0600

    season datasource

--- lib/infrastructure/datasources/isar_season_datasource.dart
+++ lib/infrastructure/datasources/isar_season_datasource.dart
@@ -10,32 +10,48 @@ class IsarSeasonDatasource extends SeasonDatasource {
   }
 
   @override
-  Future<bool> deleteSeason(Id id) {
-    // TODO: implement deleteSeason
-    throw UnimplementedError();
+  Future<bool> deleteSeason(Id id) async {
+    final isar = await db;
+    final season = await isar.seasons.filter().idEqualTo(id).findFirst();
+    if (season != null) {
+      isar.writeTxnSync(() => isar.seasons.deleteSync(id));
+      return true;
+    }
+    return false;
   }
 
   @override
-  Future<Season> getSeason(Id id) {
-    // TODO: implement getSeason
-    throw UnimplementedError();
+  Future<Season> getSeason(Id id) async {
+    final isar = await db;
+    final season = await isar.seasons.get(id);
+    if (season != null) {
+      return season;
+    }
+    throw 'La temporada no existe';
   }
 
   @override
-  Future<List<Season>> getSeasons({int limit = 10, offset = 0}) {
-    // TODO: implement getSeasons
-    throw UnimplementedError();
+  Future<List<Season>> getSeasons({int limit = 10, offset = 0}) async {
+    final isar = await db;
+    return isar.seasons.where().offset(offset).limit(limit).findAll();
   }
 
   @override
-  Future<bool> saveSeason(Season season) {
-    // TODO: implement saveSeason
-    throw UnimplementedError();
+  Future<bool> saveSeason(Season season) async {
+    final isar = await db;
+    isar.writeTxn(() async {
+      await isar.seasons.put(season);
+      await season.stats.save();
+    });
+    return true;
   }
 
   @override
-  Future<bool> updateSeason(Id id, Season season) {
-    // TODO: implement updateSeason
-    throw UnimplementedError();
+  Future<bool> updateSeason(Id id, Season season) async {
+    final isar = await db;
+    final originalSeason = await isar.seasons.get(id);
+    if (originalSeason == null) return false;
+    originalSeason.season = season.season;
+    return true;
   }
 }
