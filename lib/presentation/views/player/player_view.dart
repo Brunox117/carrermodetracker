495c307e2c41ba080ceb500f0e8c836603165b8b
Bruno Jimenez <omarjm0910@gmail.com>
2025-04-07 11:39:21 -0600

    se obtienen las estadisticas por jugador

--- lib/presentation/views/player/player_view.dart
+++ lib/presentation/views/player/player_view.dart
@@ -1,17 +1,50 @@
 import 'package:carrermodetracker/presentation/providers/players/players_provider.dart';
+import 'package:carrermodetracker/presentation/providers/stats/stats_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
-class PlayerView extends ConsumerWidget {
+class PlayerView extends ConsumerStatefulWidget {
   final String playerID;
   const PlayerView({super.key, required this.playerID});
 
   @override
-  Widget build(BuildContext context, ref) {
-    final playerAsync = ref.watch(singlePlayerProvider(int.parse(playerID)));
+  ConsumerState<ConsumerStatefulWidget> createState() => _PlayerViewState();
+}
+
+class _PlayerViewState extends ConsumerState<PlayerView> {
+  void loadStats() async {
+    ref
+        .read(statsProvider.notifier)
+        .getStatsByPlayer(id: int.parse(widget.playerID));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadStats();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final playerAsync =
+        ref.watch(singlePlayerProvider(int.parse(widget.playerID)));
+    final statsFromPlayer = ref
+        .watch(statsProvider)
+        .values
+        .where(
+          (stat) => stat.player.value!.id == int.parse(widget.playerID),
+        )
+        .toList();
     return playerAsync.when(
       data: (player) {
         return Scaffold(
+          floatingActionButton: IconButton.filledTonal(
+            onPressed: () {
+              context.push('/editplayer/${widget.playerID}');
+            },
+            icon: const Icon(Icons.edit),
+          ),
           appBar: AppBar(
             title: Text(player.name),
           ),
